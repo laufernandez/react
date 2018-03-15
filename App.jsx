7bcc547c892e252ec1f4c142c3cc043fd991806c
fquinteros <federico.quinteros@mercadolobre.com>
2018-03-15 13:15:03 -0300

    primer commit

--- App.jsx
+++ App.jsx
@@ -1,13 +1,26 @@
 import React from 'react';
 
 class App extends React.Component {
-   render() {
-      return (
-         <div>
-            Hola ITAcademy!
-         </div>
-      );
-   }
+    render() {
+        return (
+            <div class="grid" style="overflow: hidden;">
+                
+            </div>
+        );
+    }
+}
+
+class Header extends React.Component {
+    render() {
+        return (
+            <Table/>
+            <div class="row">
+
+            </div>
+        );
+    }
 }
 
 export default App;
+
+<div class="tile" id="0-0" style="font-size: 3
